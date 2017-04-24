dfc5f279ecb73bc91e9b90e3e1159579a79bdd2a
Eliot Lim <eliot.lim@gmail.com>
2017-04-24 12:19:05 +0800

    Changes to make code compileable. And fixes. Stupid mistakes include not assigning result of function call.
    Modified includes: removed image_transport
    
    RESOLVED: assignment of subscriber/publisher after declaring through
    NodeHandler.

--- src/place_recognition.cpp
+++ src/place_recognition.cpp
@@ -24,17 +24,17 @@
 #include <sensor_msgs/image_encodings.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
-#include <opencv2/cv.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
 
-ros::Subscriber input_pub;
-ros::Subscriber trigger_pub;
+const char* VERSION_STRING = "0.0.2";
+
+ros::Subscriber input_sub;
+ros::Subscriber trigger_sub;
 ros::Publisher output_pub;
 cv_bridge::CvImagePtr image_ptr;
 cv_bridge::CvImagePtr output_ptr;
 
-void image_callback (const sensor_msgs::ImageConstPtr &msg);
+void trigger_callback (const std_msgs::String &msg);
+void image_callback (const sensor_msgs::Image::ConstPtr &msg);
 
 int main (int argc, char** argv) {
     // ros::init must be the first call in main()
@@ -44,19 +44,27 @@ int main (int argc, char** argv) {
     std::string input_topic = nh.resolveName ("image_raw");
     std::string trigger_topic = nh.resolveName ("image_capture");
     std::string output_topic = nh.resolveName ("image_show");
-    uint32_t queue_size = 1;
+    uint32_t queue_size = 10;
 
-    nh.subscriber(input_topic, queue_size, image_callback);
+    ROS_INFO ("Place Recognition version %s", VERSION_STRING);
+    ROS_INFO ("image_raw maps to: %s", input_topic.c_str());
+    ROS_INFO ("image_capture maps to: %s", trigger_topic.c_str());
+    ROS_INFO ("image_show maps to: %s", output_topic.c_str());
+
+    input_sub = nh.subscribe (input_topic, queue_size, image_callback);
+    trigger_sub = nh.subscribe (trigger_topic, queue_size, trigger_callback);
+    output_pub = nh.advertise<sensor_msgs::Image> (output_topic, queue_size);
 
     ros::spin();
 }
 
-void trigger_callback (const std_msgs::Empty &msg) {
+void trigger_callback (const std_msgs::String &msg) {
     //TODO: Implement DB Updating and Tagging
     ROS_INFO ("DB Insertion TRIGGERED");
 }
 
 void image_callback (const sensor_msgs::ImageConstPtr &msg) {
+    ROS_INFO ("received ImageMsg");
     try {
         image_ptr = cv_bridge::toCvCopy (msg, sensor_msgs::image_encodings::BGR8);
         output_ptr = cv_bridge::toCvCopy (msg, sensor_msgs::image_encodings::BGR8);
